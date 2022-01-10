afd97bc45309814776a8f096527785a766bb21ef
jesusalc <jesusalc@gmail.com>
2022-01-10 17:17:49 +0100

    moved all info to about route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/about'
   devise_for :users
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
